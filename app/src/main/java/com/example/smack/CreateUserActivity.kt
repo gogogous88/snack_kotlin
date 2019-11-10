b3951e6e528e3e76c7e9d51aa36a72218a781f8f
Mark Mu <mark.mu@totalbrain.com>
2019-11-09 23:41:06 -0800

    imageView.setImageResource to set image resource setBackgroundColor to set color of image. resource.getIdentifier string name folder name, folder type to get route of image. Random().nextInt to setup a random number

--- app/src/main/java/com/example/smack/CreateUserActivity.kt
+++ app/src/main/java/com/example/smack/CreateUserActivity.kt
@@ -1,10 +1,16 @@
 package com.example.smack
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar = "profileDefault"
+    var avatarColor = "[0.5,0.5,0.5,1]"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,11 +18,35 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun generateAvatarImage(view: View){
+        val random = Random()
+        val color = random.nextInt(2)
+        val avatar = random.nextInt(28)
 
+        if(color == 0){
+            userAvatar = "light$avatar"
+        } else {
+            userAvatar = "dark$avatar"
+        }
+
+        val resourceId = resources.getIdentifier(userAvatar, "drawable", packageName)
+        createAvatarImageView.setImageResource(resourceId)
     }
 
     fun generateColorClicked(view: View){
+        val random = Random()
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
 
+        createAvatarImageView.setBackgroundColor(Color.rgb(r,g,b))
+
+        val savedR = r.toDouble() / 255
+        val savedG = r.toDouble() / 255
+        val savedB = r.toDouble() / 255
+
+        avatarColor = "[$savedR, $savedG, $savedB, 1]"
+
+        println(avatarColor)
     }
 
     fun createUserClicked(view: View){
